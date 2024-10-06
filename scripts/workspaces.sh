c2a8e57a5226b91bf5b7379c6145c29999afe804
Herbert <herbert@groot.jebbink.nl>
2024-10-06 15:01:49 +0200

    Automatic update

--- scripts/workspaces.sh
+++ scripts/workspaces.sh
@@ -4,14 +4,23 @@ rm -Rf /workspaces/*
 
 cd /workspaces
 
-tar zxf ~/ace/archives/workspace.ace.tar.gz
 tar zxf ~/ace/archives/workspace.mq.tar.gz
+tar zxf ~/ace/archives/workspace.ace.tar.gz
 tar zxf ~/ace/archives/workspace.db2.tar.gz
 tar zxf ~/ace/archives/workspace.eclipse.tar.gz
 
 mkdir -p /ace/log/eclipse
 
-nohup /opt/mqsi/ace toolkit       -data /workspaces/ace     < /dev/null > /ace/log/eclipse/ace.log 2>&1 &
 #nohup MQExplorer                  -data /workspaces/mq      < /dev/null > /ace/log/eclipse/mq.log  2>&1 &
+nohup /opt/mqsi/ace toolkit       -data /workspaces/ace     < /dev/null > /ace/log/eclipse/ace.log 2>&1 &
 nohup /opt/sdp/datastudio/eclipse -data /workspaces/db2     < /dev/null > /ace/log/eclipse/db2.log 2>&1 &
 nohup /opt/eclipse/eclipse        -data /workspaces/eclipse < /dev/null > /ace/log/eclipse/eclipse.log 2>&1 & 
+
+
+EXT-IN inkomend
+EXT-OUT uitgaand
+
+ESB
+
+INT-IN  inkomend
+INT-OUT	 uitgaand
