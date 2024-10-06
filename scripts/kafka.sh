8fb825b63ed7cd9eff87bc04e6989a71edd156e6
Herbert Groot Jebbink <herbert@groot.jebbink.nl>
2024-10-06 13:44:25 +0200

    Automatic update

--- scripts/kafka.sh
+++ scripts/kafka.sh
@@ -11,8 +11,9 @@ rmdir kafka_2.13-3.8.0
 mkdir /kafka/logs
 
 nohup /kafka/bin/zookeeper-server-start.sh config/zookeeper.properties < /dev/null > logs/kafka_zookeeper.txt 2>&1 &
-sleep 1
+sleep 2
 nohup /kafka/bin/kafka-server-start.sh     config/server.properties    < /dev/null > logs/kafka_server.txt    2>&1 &
-sleep 1
+sleep 2
 
-bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic IIB01
+/kafka/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic helloRequest
+/kafka/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic helloResponse
--- test/hello/kafka.sh
+++ test/hello/kafka.sh
@@ -1,3 +1,5 @@
 #/bin/sh
 
-cat ~/ace/test/hello/hello.xml | /kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic IIB01
+cat ~/ace/test/hello/kafka.xml | /kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic helloRequest
+
+nohup sleep 5 && kcat -C -b localhost:9092 -t helloResponse -o -1 -e > /ace/test/hello/kafka.xml &
